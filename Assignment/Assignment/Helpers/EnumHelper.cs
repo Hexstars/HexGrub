f87854261f3b1806ac9cfd2ad9adae143b7b2fde
Hexstars <baotnc19@gmail.com>
2024-11-27 12:40:31 +0700

    Order view changes

--- Assignment/Assignment/Helpers/EnumHelper.cs
+++ Assignment/Assignment/Helpers/EnumHelper.cs
@@ -1,6 +1,18 @@
-﻿namespace Assignment.Helpers
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Assignment.Helpers
 {
     public class EnumHelper
     {
+        public static string GetDisplayName(Enum enumValue)
+        {
+            var displayAttribute = enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>();
+
+            return displayAttribute?.Name ?? enumValue.ToString();
+        }
     }
 }
